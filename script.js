3c977f44fd64fa1e54d446da711f590b6b42f82e
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-14 15:02:50 -0400

    Update script.js
    made a variable for numbers 0-9

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ var toUpper = function (x) {
 
 upperCase = lowerCase.map(toUpper);
 
+// making a variable for numbers 0-9
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
