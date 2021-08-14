1626a1218375c2b88767b595db6ff5586e8d24c2
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-14 14:57:12 -0400

    Update script.js
    Wrote out what the variable "lowerCase" would equal to, which would be each letter in the alphabet in lower case

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var confirmNumber;
 var confirmSpecial;
 var userChoices;
 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
