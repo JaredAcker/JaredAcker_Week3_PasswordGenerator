39e307e36d76a90f65cfab5add705eb9e96eccbc
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-14 15:01:17 -0400

    Update script.js
    made a function to upper case lower cased letters.

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ var confirmNumber;
 var confirmSpecial;
 var userChoices;
 
+// lowerCase variable is equal to the entire alphabet in lower case.
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// making a function to upper case lower cased letters.
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+
+upperCase = lowerCase.map(toUpper);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
