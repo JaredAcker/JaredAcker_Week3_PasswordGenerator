38824cb1d657b842833d60c4c1670137b01c45a4
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-14 14:53:32 -0400

    Update script.js
    created variables for password length, lowercase letters, uppercase letters, numbers, special characters and user choices.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// variables for password length, lowercase letters, uppercase letters, numbers, special characters and user choices.
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
